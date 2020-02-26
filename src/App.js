5f71b5ead3fee425a6cdce81bfb5365e953eff14
Raphael Freitas <rrabbi@hotmail.com>
2020-02-27 00:47:47 +0100

    WIP - FCC Drum Machine

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="flex-container flex-content-center">
+        <div id="drum-machine" className="machine-container">
+          <div className="machine-header">My Drum Machine</div>
+          <div className="controls-container">
+            <div className="pads-wrapper grid-container grid-3-col">
+              <div className="drum-pad">Q</div>
+              <div className="drum-pad">W</div>
+              <div className="drum-pad">E</div>
+              <div className="drum-pad">A</div>
+              <div className="drum-pad">S</div>
+              <div className="drum-pad">D</div>
+              <div className="drum-pad">Z</div>
+              <div className="drum-pad">X</div>
+              <div className="drum-pad">C</div>
+            </div>
+            <div className="display-wrapper">
+              <div className="display-title">You are playing</div>
+              <div className="display">Test audio</div>
+            </div>
+          </div>
+
+        </div>
+      </div>
     </div>
   );
 }
